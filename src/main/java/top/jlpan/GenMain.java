3316a16402679a5e8c22703c4fb8ad23aad80c16
panliang <18723083600@163.com>
2020-04-17 20:41:50 +0800

    新增代码合并

--- src/main/java/top/jlpan/GenMain.java
+++ src/main/java/top/jlpan/GenMain.java
@@ -22,11 +22,12 @@ import java.util.Scanner;
  * 选择需要的表执行即可
  * @Date 2020/4/15 11:19
  */
-@SuppressWarnings("all" )
+@SuppressWarnings("all")
 public class GenMain {
 
     private static String origin = "/*gen_origin_sigin*/";
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
 
         /* 通用参数配置 */
         GenConfig.url = "jdbc:mysql://139.199.179.144:3306/ruoyi?usdeUnicode=true&characterEncoding=UTF-8&serverTimezone=UTC";
@@ -44,12 +45,12 @@ public class GenMain {
 
         /* 自定义数据声明 */
         HashMap<String, Object> fillMap = new HashMap<>();
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss" );
-        fillMap.put("createDate" , format.format(new Date()));
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        fillMap.put("createDate", format.format(new Date()));
         /* FIXME 作者  */
-        fillMap.put("author" , "panliang" );
-        fillMap.put("version" , "1.0" );
-        fillMap.put("namespace" , "top.jlpan.project.upms" );
+        fillMap.put("author", "panliang");
+        fillMap.put("version", "1.0");
+        fillMap.put("namespace", "top.jlpan.project.upms");
         fillMap.put("origin", origin);
 
         /* 表数据声明 */
@@ -91,68 +92,72 @@ public class GenMain {
      * @param path
      * @param sw
      */
-    static void writeFile(String path, StringWriter sw) {
-        try {
-            String newString = sw.toString();
-            System.out.println("路径:" + path + " 生成中...." );
-            File file = new File(path);
-            if (!file.exists()) {
-                System.out.println("文件路径:" + path + " 不存在,正在新建..." );
-                if (!file.getParentFile().exists()) {
-                    file.getParentFile().mkdirs();
-                    file.createNewFile();
+    static void writeFile(String path, StringWriter sw) throws IOException {
+
+        String newString = sw.toString();
+        System.out.println("路径:" + path + " 生成中....");
+        File file = new File(path);
+        if (!file.exists()) {
+            System.out.println("文件路径:" + path + " 不存在,正在新建...");
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+                file.createNewFile();
+            }
+        } else {
+            while (true) {
+                System.out.println("文件路径:" + path + "已存在,选择覆盖,取消,根据" + origin + "合并或重命名？");
+                System.out.println("1:覆盖 2:取消 3:合并 4:重命名");
+                Scanner scanner = new Scanner(System.in);
+                String an = scanner.nextLine();
+                if ("1".equals(an)) {
+                    System.out.println("覆盖中...");
+                    file.delete();
+                    System.out.println("覆盖完毕！");
+                    break;
                 }
-            } else {
-
-
-                FileInputStream stream = new FileInputStream(file);
-
-                int size=stream.available();
-
-                byte[] buffer=new byte[size];
-
-                stream.read(buffer);
-
-                stream.close();
-                String string = new String(buffer);
-                int originIndex = string.lastIndexOf(origin);
-                String other = string.substring(originIndex + origin.length());
-                String tihuan = newString.substring(0, originIndex + origin.length() + 1);
-
-                while (true) {
-                    System.out.println("文件路径:" + path + "已存在,是否选择覆盖，取消或重命名？" );
-                    System.out.println("1:覆盖 2:取消 3:重命名" );
-                    Scanner scanner = new Scanner(System.in);
-                    String an = scanner.nextLine();
-                    if ("1".equals(an)) {
-                        System.out.println("覆盖中..." );
-                        file.delete();
-                        System.out.println("覆盖完毕！" );
-                        break;
-                    }
-                    if ("2".equals(an)) {
-                        System.out.println("跳过执行！" );
+                if ("2".equals(an)) {
+                    System.out.println("跳过执行！");
+                    return;
+                }
+                if ("3".equals(an)) {
+                    // 读取旧文件
+                    FileInputStream stream = new FileInputStream(file);
+                    int size = stream.available();
+                    byte[] buffer = new byte[size];
+                    stream.read(buffer);
+                    stream.close();
+                    String oldString = new String(buffer);
+                    int oldIndex = oldString.lastIndexOf(origin);
+                    int newIndex = newString.lastIndexOf(origin);
+                    if (oldIndex == -1 || newIndex == -1) {
+                        System.out.println("不存在" + origin + ",跳过执行！");
                         return;
                     }
-                    if ("3".equals(an)) {
-                        int index = path.lastIndexOf("/" );
-                        String fileName = path.substring(index + 1);
-                        String head = path.substring(0, index + 1);
-                        String copy = head + "Copy" + fileName;
-                        file = new File(copy);
-                        System.out.println("已重命名！" );
-                        break;
-                    }
-                    System.out.println("输入错误，请重新输入！" );
+
+                    String oldNeed = oldString.substring(oldIndex + origin.length());
+                    String newNeed = newString.substring(0, newIndex + origin.length() + 1);
+                    newString = newNeed + oldNeed;
+                    System.out.println("已合并！");
+                    break;
                 }
+                if ("4".equals(an)) {
+                    int index = path.lastIndexOf(".");
+                    String fileName = path.substring(index);
+                    String head = path.substring(0, index);
+                    String copy = head + "Copy" + fileName;
+                    file = new File(copy);
+                    System.out.println("已重命名！");
+                    break;
+                }
+                System.out.println("输入错误，请重新输入！");
             }
-            OutputStream out = new FileOutputStream(file);
-            out.write(sw.toString().getBytes());
-            out.flush();
-            out.close();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+
+        OutputStream out = new FileOutputStream(file);
+        out.write(newString.getBytes());
+        out.flush();
+        out.close();
+
     }
 
 }
